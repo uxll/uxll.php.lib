52e361dce6035fa0c79030980f7095f16a7722be
tiananwei <uxll@qq.com>
2012-08-27 17:12:48 +0800

    add language

---
+++ lib/uxll/language/en.php
@@ -0,0 +1,36 @@
+<?php
+return array(
+	'redirect_time_decrease' => 'The page will redirect to <a href="{{$url}}">{{$url}}</a> after <span id="redirect_time_decrease">{{$delay}}</span> seconds...',
+	'back_to' => 'BACK TO',
+	'home' => 'HOME',
+	'success' => ':: success ::',
+	
+	'failed' => ':: failed ::',
+	'page' => array(
+		'of' => ' of ',
+		'previous' => 'previous',
+		'next' => 'next'
+	),
+	'FrontControl' => array(
+		'accessdeny' => 'Access deny',
+		'routeerror' => 'route Action error',
+		'classnotfound' => 'This class does not inherit from IControl'	
+	
+	)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+);
